72320c8f9deda50babc21cff6ed01010b86342d6
gulshan25 <g.r.tanny@gmail.com>
2022-03-29 20:23:08 +0600

    javaScript

--- test.js
+++ test.js
@@ -11,3 +11,42 @@ y = 6;
 z = x % y;
 
 console.log(z);
+
+//  muted - objects and arrays. it is a type of variable that can be changed. muted - let use kore hobe. //
+//  immuted - primitives values(numbers, strings, booleans, null, undefined). it can't be changed. //
+// immuted - always const use kore hobe let na. object and array teh const use kore hobe. //
+
+// object-er bhitore kono function thaka ke method bole. //
+// literals - {} //
+
+let immutableString = "Hello";
+
+immutableString = immutableString + " " + " World ";
+
+console.log(immutableString);
+
+const person = {
+    fname: "Gulshan",
+    lname: "Rahman",
+    age: 00,
+};
+
+let txt = "";
+for (let x in person) {
+    txt += person[x] + " ";
+}
+
+console.log(txt);
+
+const car = {
+    name: "volvo",
+    weight: 100,
+    color: "black",
+};
+
+let text = "";
+for (let x in car) {
+    text += car[x] + " ";
+}
+
+console.log(text);
