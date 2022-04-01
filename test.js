00a9d27abb60e3413179204d04d1858c8571c198
gulshan25 <g.r.tanny@gmail.com>
2022-04-01 16:18:43 +0600

    javaScript

--- test.js
+++ test.js
@@ -69,6 +69,9 @@ const cars = {
     color: "black",
 };
 
+console.log(Object.values(cars));
+console.log(JSON.stringify(cars));
+
 // let text = "";
 // for (let x in cars) {
 //     text += cars[x] + " ";
@@ -78,7 +81,7 @@ cars.nationality = "Bangladesh";
 
 console.log(cars);
 
-const myObj = {
+const myObj1 = {
     name: "john",
     age: 30,
     cars: {
@@ -88,6 +91,123 @@ const myObj = {
     },
 };
 
+console.log(myObj1 instanceof Object);
+
+const myObj = {
+    name: "john",
+    age: 30,
+    cars: [
+        { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+        { name: "BMW", models: ["320", "X3", "X5"] },
+        { name: "Fiat", models: ["500", "Panda"] },
+    ],
+};
+
+for (let i in myObj.cars) {
+    console.log(i);
+}
+
+console.dir(myObj);
+
+let p1 = "cars";
+let p2 = "car1";
+let p3 = "car2";
+let p4 = "car3";
+
+console.log(myObj[p1][p2]);
+console.log(myObj[p1][p3]);
+console.log(myObj[p1][p4]);
+
 console.log(myObj);
 console.log(myObj.cars);
 console.log(myObj.cars.car3);
+console.log(myObj.cars["car1"]);
+console.log(myObj["cars"]);
+console.log(myObj["cars"]["car2"]);
+console.log(Object.values(cars));
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    language: "en",
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    },
+};
+
+console.log(person.fullName);
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    language: "",
+    set lang(lang) {
+        this.language = lang;
+    },
+};
+
+person.lang = "bn";
+
+console.log(person);
+
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    },
+};
+
+console.log(person1.fullName());
+
+const student = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 100,
+    fullName: function() {
+        return this.firstName + " " + this.lastName + " " + this.id;
+    },
+};
+
+console.log(student);
+
+const student1 = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 100,
+    get fullName() {
+        return this.firstName + " " + this.lastName + " " + this.id;
+    },
+};
+
+console.log(student1);
+
+function Person(firstName, lastName) {
+    // construction function
+
+    this.firstName = first;
+    this.lastName = last;
+    this.fullName = function() {
+        return this.firstName + " " + this.lastName;
+    };
+}
+
+const sumit = new Person("sumit", "saha", 35);
+
+console.log(sumit.fullName());
+
+const fruits = new Map();
+
+fruits.set("bananas", 300);
+fruits.set("apples", 500);
+fruits.set("oranges", 200);
+
+fruits.set("oranges", 800);
+fruits.get("bananas", 300);
+fruits instanceof Map;
+
+console.log(fruits);
+
+(function() {
+    console.log("I am a");
+})(); // self invoking function. invoking and calling same. // (); = IIFE - immediately invoked function expression.
